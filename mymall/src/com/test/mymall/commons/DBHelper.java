be7c8ea8a94cbeb1a3b5a426136e541178818af1
Administrator <Administrator@MSDN-SPECIAL>
2018-10-26 15:28:48 +0900

    [강승우] 기본 클래스 생성
    1. 기본클래스 생성
    2. AddMemberController.doGet()구현

---
+++ mymall/src/com/test/mymall/commons/DBHelper.java
@@ -0,0 +1,17 @@
+package com.test.mymall.commons;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DBHelper {
+	
+	public static Connection getConnection() throws Exception {
+		return null;
+	}
+	
+	public static void close(ResultSet rs, Statement stmt, Connection conn) {
+		if(rs != null) {
+		}
+	}
+}
--- mymall/src/com/test/mymall/web/IndexController.java
+++ mymall/src/com/test/mymall/web/IndexController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class IndexController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("IndexController.doGet");
+		System.out.println("IndexController.doGet()");
 		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);
 	}
 
